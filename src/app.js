0e5ceee0e8f7de1a41efd40a1e52cd9b23f42750
Himanshu <92033781+HimanshuVerma5@users.noreply.github.com>
2026-01-30 01:23:06 +0530

    exploring get post and delete

--- src/app.js
+++ src/app.js
@@ -2,17 +2,21 @@ const express=require("express");
 
 const app=express();
 
-
-
-app.use("/hello",(req,res)=>{
-   res.send("Hello hello hello");
+app.get("/user",(req,res)=>{
+  res.send({ firstName:"Himanshu",lastName :"Verma" });
 });
+app.post("/user",(req,res)=>{
+  res.send("Data Is In DataBase");
+});
+app.delete("/user",(req,res)=>{
+  res.send("Deleted Succesfully");
+});
+
 app.use("/test",(req,res)=>{
  res.send("Hello from server...");
 });
-app.use("/",(req,res)=>{
-    res.send("Hello from the dashboard");
-});
+
+
 
 app.listen(7777,()=>{
     console.log("Server is succesfully runs");
